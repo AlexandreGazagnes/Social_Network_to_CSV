522a3a4339d4c1cfbe3010ac8122fb1122fddf8b
Alexandre GAZAGNES <a_syoez@yahoo.fr>
2017-06-26 22:44:17 +0200

    Update search_linkedin_premium_v1.0_lts.py

--- search_linkedin_premium_v1.0_lts.py
+++ search_linkedin_premium_v1.0_lts.py
@@ -1,170 +1,165 @@
-def traitement_linkedin_premium_search_juin_2017(chaine):
-  count=0
+def traitement_search_linkedin_premium(chaine):
 
-  # 1/ on enleve les blancs et les ***** - OK
-  chaine = chaine.replace("*****", "").replace("****", "")
-  
-  count+=1; print("Manip n* {}".format(count))
-  # --------------------------------------------
+    count=0
 
-  # 2/ on en fait une liste  en supprimant les lignes vides et en rajoutant une ligne - OK
-  li = chaine.split("\n"); li.insert(1,"NFA"); liste =  [i.strip() for i in li if i]
+    def donner_numero_manipulation(count):
+        count+=1
+        print("Manip n* {}".format(count))
+        return count
 
-  count+=1; print("Manip n* {}".format(count))
-  # --------------------------------------------
-   
-  # 3/ on en fait une liste de liste - OK 
-  liste2, sous_liste = list(), list()
+    def supprimer_les_etoiles(chaine): # On enleve les ***** - OK
+        chaine = chaine.replace("*****", "").replace("****", "")
+        return chaine
 
-  for j in liste:
-    if j[:12] == 'Sélectionner': 
-      liste2.append(list(sous_liste))
-      sous_liste = list()
-    else : 
-      sous_liste.append(j)
-      
-  count+=1; print("Manip n* {}".format(count))
-  # --------------------------------------------
+    def rajouter_1er_ligne_et_supprimer_blanck(chaine) :# OK
+        liste = chaine.split("\n"); 
+        liste.insert(1,"NFA"); 
+        liste =  [i.strip() for i in liste if i]
+        return liste
+     
+    def creer_liste_de_liste(liste): # OK 
+        liste2, sous_liste = list(), list()
 
-  # 4/ suppression 1ere valeur et creation d'un ID - OK
-  for i,j in enumerate(liste2):
-    liste2[i][0] = str(i) # --> forcément besoin de str et pas de int ???
+        for j in liste:
+            if j[:12] == 'Sélectionner': 
+                liste2.append(list(sous_liste))
+                sous_liste = list()
+            else : 
+                sous_liste.append(j)
+        return liste2           
 
-  count+=1; print("Manip n* {}".format(count))
-  # --------------------------------------------
-  
-  # 5/ separation nom n* réseau / On part la dessus mais on a un Gros PB ! Qui de si la personne n'est pas en réseau 1 / 2 / 3 ???? - OK MAIS à REVOIR
-  for i,_ in enumerate (liste2): 
-    if "\t" in liste2[i][1]: liste2[i][1] = liste2[i][1][:liste2[i][1].find("\t")].strip()
+    def creation_ID(liste): # OK
+        for i,j in enumerate(liste):
+            liste[i][0] = str(i) # --> forcément besoin de str et pas de int ???
+        return liste
 
-  count+=1; print("Manip n* {}".format(count))
-  # --------------------------------------------  
+    def separer_nom_num_reseau(liste): # OK +/-
+        #On part la dessus mais on a un Gros PB ! Qui de si la personne n'est pas en réseau 1 / 2 / 3 ???? - OK MAIS à REVOIR
+        for i,_ in enumerate(liste): 
+            if "\t" in liste[i][1]: 
+                liste[i][1] = liste[i][1][:liste[i][1].find("\t")].strip()  
+        return liste
 
-  # 6/ suppression Titre uselless - OK
-  for i,j in enumerate(liste2) : liste2[i].pop(2)
-  
-  count+=1; print("Manip n* {}".format(count))
-  # -------------------------------------------- 
-  
-  # 7/ suppression pays - secteur - OK
-  for i,j in enumerate(liste2) : liste2[i].pop(3)
-
-  count+=1; print("Manip n* {}".format(count))
-  # --------------------------------------------
-  
-  # 8/ Séparation métier / ssté / ancienneté - OK
-  char = "chez" # sachant que char peut aussi valoir @ ou at
-  for i, j in enumerate(liste2): # separation poste ssté
-    if char in  liste2[i][3] : 
-      elem = liste2[i][3][liste2[i][3].find(char):]
-      liste2[i].insert(4, elem) 
-      liste2[i][3] = liste2[i][3][:liste2[i][3].find(char)]
-    else: 
-      liste2[i].insert(4, "?") 
-      
-  for i, j in enumerate(liste2): # on enleve le char
-    if char in  liste2[i][4] : liste2[i][4] = liste2[i][4].replace(char, "")
-      
-  for i, j in enumerate(liste2): # spéaration de la date
-    nb =-1
-    for k in liste2[i][4] : 
-      if k.isdigit():
-        nb = liste2[i][4].find(k)
-        break
-    if nb >0 : 
-      elem = liste2[i][4][nb:]
-      liste2[i].insert(5, elem)
-      liste2[i][4] = liste2[i][4][:nb]
-    else : 
-      liste2[i].insert(5, "?") # ---> WTF
-            
-   # EST-CE QU'il FAUT AUSSI PRENDRE EN COMPTE LE "AT" OU LE "@"???
-   
-  for i,j in enumerate(liste2):
-    liste2[i][5] = liste2[i][5][:4] if liste2[i][5] != "?" else "?"
-   
-  count+=1; print("Manip n* {}".format(count))
-  # --------------------------------------------
-  
-  # 9/ Enlever le "France • Construction "
-  char = ","
-  for i, j in enumerate(liste2): #
-    if char in  liste2[i][2] : 
-      liste2[i][2] = liste2[i][2][:liste2[i][2].find(char)]
-
-  count+=1; print("Manip n* {}".format(count))
-  # --------------------------------------------
-
-
- # 10/ séparer formation abnnée de diplome
-  for i, j in enumerate(liste2):
-    for k,l in enumerate(liste2[i]):
-      if l == "Formation" : 
-        elem = liste2[i][k+1]
-        liste2[i].insert(6,elem[:-11])
-        liste2[i].insert(7,elem[-4:])
-
-        break
-      
-    if "Formation" not in j:
-      liste2[i].insert(6,"?")
-      liste2[i].insert(7,"?")
-
-
-  count+=1; print("Manip n* {}".format(count))
-  # --------------------------------------------
-  
-  
-  # 11 /  supprimer "relation en commun" et "Enregistrer dans projet"
-  
-  nb = -1
-  for i, j in enumerate(liste2): 
-    for k,l in enumerate(liste2[i]):
-      if ("relations en commun" or "relation en commun" ) in l :
-        nb = k
-  if nb>0 : liste2[i].pop(nb)
-        
-  nb = -1      
-  for i, j in enumerate(liste2): 
-    for k,l in enumerate(liste2[i]):
-      if "Enregistrer dans" in l :
-        nb = k
-  if nb>0 : liste2[i].pop(nb)
-  
-  nb = -1      
-  for i, j in enumerate(liste2): 
-    for k,l in enumerate(liste2[i]):
-      if "Plus" == l :
-        nb = k
-  if nb>0 : liste2[i].pop(nb)
-
-
-  # 11 / On strip le reste 
-  for i, j in enumerate(liste2):
-    lon = len(j)  
-    liste2[i][8] = "; ".join(liste2[i][8: ])
-    liste2[i][9:] = []
- 
-  count+=1; print("Manip n* {}".format(count))  
-  # --------------------------------------------
-  
-
-  # CONTROLES 
-  liste_len = list()
-  for i,j in enumerate(liste2):
-    liste_len.append((len(j),i))
+    def suppression_titre_sous_nom_useless(liste): # OK
+        for i,j in enumerate(liste) : 
+            liste[i].pop(2)
+        return liste
     
+    def supprimer_actruellement(liste): # OK
+        for i,j in enumerate(liste) : 
+            liste[i].pop(3)
+        return liste
+    
+    def separer_metier_sste_anciennete(liste): # OK
+        
+        char = "chez" # sachant que char peut aussi valoir @ ou at
+        
+        for i, j in enumerate(liste): # separation poste ssté
+            if char in  liste[i][3] : 
+                elem = liste[i][3][liste[i][3].find(char):]
+                liste[i].insert(4, elem) 
+                liste[i][3] = liste[i][3][:liste[i][3].find(char)]
+            else: 
+                liste[i].insert(4, "?") 
+                
+        for i, j in enumerate(liste): # on enleve le char
+            if char in  liste[i][4] : liste[i][4] = liste[i][4].replace(char, "")
+                
+        for i, j in enumerate(liste): # spéaration de la date
+            nb =-1
+            for k in liste[i][4] : 
+                if k.isdigit():
+                    nb = liste[i][4].find(k)
+                    break
+            if nb >0 : 
+                elem = liste[i][4][nb:] # on sépare toute la date : 2014 -aujourd'hui
+                liste[i].insert(5, elem)
+                liste[i][4] = liste[i][4][:nb]
+            else : 
+                liste[i].insert(5, "?") # ---> WTF        
+             # EST-CE QU'il FAUT AUSSI PRENDRE EN COMPTE LE "AT" OU LE "@"???
+        
+        for i,j in enumerate(liste): # on ne garde que les 4 1ers chiffres, date début
+            liste[i][5] = liste[i][5][:4] if liste[i][5] != "?" else "?"
+        
+        return liste
 
-  # PAS DE VIGULES DANS UN CSV 
-  for i,j in enumerate(liste2):
-    for k,l in enumerate(liste2[i]):
-      liste2[i][k] = liste2[i][k].replace(",", " - ") 
+    def supprimer_pays_secteur(liste): # OK
+        char = ","
+        for i, j in enumerate(liste): #
+            if char in  liste[i][2] : 
+                liste[i][2] = liste[i][2][:liste[i][2].find(char)]
+        return liste
+
+    def separer_formation_annee_diplome(liste): # OK
+        for i, j in enumerate(liste):
+            for k,l in enumerate(liste[i]):
+                if l == "Formation" : 
+                    elem = liste[i][k+1]
+                    liste[i].insert(6,elem[:-11])
+                    liste[i].insert(7,elem[-4:])
+                    break              
+            if "Formation" not in j:
+                liste[i].insert(6,"?")
+                liste[i].insert(7,"?")
+        return liste
+    
+    def supprimer_champs_inutiles(liste): # EN COURS   
+        for chaine in ['relations en ', 'relation en', 'Enregistrer dans']:
+            nb = -1
+            for i, j in enumerate(liste): 
+                for k,l in enumerate(liste[i]):
+                    if chaine in l :
+                        nb = k    
+        
+        nb = -1      
+        for i, j in enumerate(liste): 
+            for k,l in enumerate(liste[i]):
+                if "Plus" == l :
+                    nb = k
+        if nb>0 : liste[i].pop(nb)
+
+        return liste
+
+    def stripper_le_reste_des_champs(liste):
+        for i, j in enumerate(liste):
+            lon = len(j)
+            liste[i][8] = "; ".join(liste[i][8: ])
+            liste[i][9:] = []
+        return liste
+ 
+    def formatage_csv(liste):
+        for i,j in enumerate(liste):     # PAS DE VIGULES DANS UN CSV 
+            for k,l in enumerate(liste[i]):
+                liste[i][k] = liste[i][k].replace(",", " - ") 
+
+        chaine = str()     # COMPILE EN CSV
+        for i in liste:     
+            chaine += ", ".join(i)
+            chaine+="\n"
+        
+        return chaine
+    
+    ########################################################################
 
 
-  # COMPILE EN CSV
-  retour = str()
-  for i in liste2:     
-    retour += ", ".join(i)
-    retour+="\n"
-  
-  return retour
+    element = chaine
+    del chaine
+
+
+    for fonction in [
+        supprimer_les_etoiles, 
+        rajouter_1er_ligne_et_supprimer_blanck, 
+        creer_liste_de_liste, creation_ID, separer_nom_num_reseau, 
+        suppression_titre_sous_nom_useless, 
+        supprimer_actruellement, 
+        separer_metier_sste_anciennete,
+        supprimer_pays_secteur,
+        separer_formation_annee_diplome, 
+        supprimer_champs_inutiles, 
+        stripper_le_reste_des_champs, 
+        formatage_csv] : 
+            element = fonction(element)
+            count = donner_numero_manipulation(count)
+
+    return element
