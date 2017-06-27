0819c88a6c3e55389a9f00feaec0cdc491f3aeac
Alexandre GAZAGNES <a_syoez@yahoo.fr>
2017-06-27 15:28:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
 
+# ASKING TOOLS, 
+
+"""
+tool_list = ask search tool (google, lnkedin, viadeo, doyoubuzz, twitter, xing, facebook)
+
+tool_list = {google :True, linkedin = True, viadeo = False...}
+
+if  linkedin in tool_list : tool_list[linkedin] = ask_kind_acess_linkedin(Free, Premium, Rexcruiter)
+
+if  viadeo in tool_list : tool_list[viadeo] = ask_kind_acess_linkedin(Free, Premium, Rexcruiter)
+
+tool_list = {google :True, linkedin = Recruiter, viadeo = Premium...}
+
+
+# ASKING search
+
+search = ask search creitera (bool, companies, job, eductation, years_of_xp, localiation)
+
+search = {bool:"commercial* OR business", companies: ["philips", "ineo", "hager", "artelia"], job=None, localisation: "idf"}
+
+
+# parsing each tool 
+
+for tool in tool_list : 
+  web browser()
+  id, passwrd = ask id, passwrd
+  connect(tool, id, passwrd)
+  launch_search(tool, search)
+  import_result_csv()
+  store_result_csv()
+
+# agregate whole results 
+
+for reuslt in results:
+  concatenate in a single (doc)
+  dedoublonate()
+  merge()
+  print csv
+
+# more valueable information
+  concatenate in a single (doc)
+  smart dedoublonate()
+  merge()
+  add informaton
+  print csv
+
+
+
+
+"""
